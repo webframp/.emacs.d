8500dd5c196c065c16947078847cebc529cef12c
Sean Escriva <sean.escriva@gmail.com>
2014-03-10 22:14:16 -0700

    no line highlight thanks

--- init.el
+++ init.el
@@ -33,6 +33,7 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
 
 ;; company mode for auto complete
 (add-hook 'after-init-hook 'global-company-mode)
