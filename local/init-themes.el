fa7e7e6449f0413705b279a85306a0cb783308c3
Sean Escriva <sean.escriva@gmail.com>
2014-08-10 20:51:58 -0400

    use different theme on osx

--- local/init-themes.el
+++ local/init-themes.el
@@ -1,7 +1,13 @@
 ;;; package --- color theme setup
 ;;; Commentary:
 ;;; Code:
-(load-theme 'base16-solarized t)
+
+(when *is-windows*
+  (load-theme 'base16-solarized t))
+
+;; use tomorrow theme on osx
+(when *is-osx*
+  (load-theme 'base16-tomorrow t))
 
 (provide'init-themes)
 
